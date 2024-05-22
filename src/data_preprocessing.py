7f6cb822729ba5ffa95b1eac740d12e53141cd9c
balooop <pavan.baloo@gmail.com>
2024-05-22 01:13:50 -0400

    updated data preprocessing. also modularized, added config, added main, and cleaned up code

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import numpy as np
+import logging
+import traceback
 
-def clean_data(csv_path):
+# init logging
+logging.basicConfig(level = logging.INFO, format='%(asctime)s - %(levelname)s -  %(message)s')
+    
+# define column variables
+COLUMN_MATCHWEEK = 'Matchweek'
+COLUMN_HOME_XG = 'Home XG'
+COLUMN_AWAY_XG = 'Away XG'
+COLUMN_SCORE = 'Score'
+COLUMN_HOME_GOALS = 'Home Goals'
+COLUMN_AWAY_GOALS = 'Away Goals'
+
+def clean_data(raw_data_path):
     """
     loads data from csv and preprocess to fix types
     args: 
@@ -9,47 +22,50 @@ def clean_data(csv_path):
     returns:
         played, unplayed (pd.DataFrame): played & unplayed games. "Home Goals" & "Away Goals" for unplayed games are NaN
     """
-    # initialize DataFrame for prem_data
-    prem_data = pd.read_csv(csv_path)
-    print("prem_data: \n", prem_data.head())
+    raw_data = pd.read_csv(raw_data_path)
+    logging.info(f"Data loaded successfully from {raw_data_path} in clean_data")
 
-    # adjust types
-    prem_data['Matchweek'] = prem_data['Matchweek'].astype(int)
-    prem_data.loc[:, 'Home XG'] = prem_data['Home XG'].astype(float)
-    prem_data.loc[:, 'Away XG'] = prem_data['Away XG'].astype(float)
-    print("prem_data: \n", prem_data.head())
+    # initialize DataFrame for raw_data
+    try: 
+        # adjust types
+        raw_data[COLUMN_MATCHWEEK] = raw_data[COLUMN_MATCHWEEK].astype(int)
+        raw_data[COLUMN_HOME_XG] = raw_data[COLUMN_HOME_XG].astype(float)
+        raw_data[COLUMN_AWAY_XG] = raw_data[COLUMN_AWAY_XG].astype(float)
+        
+        # separate for processing scores
+        played = raw_data[raw_data[COLUMN_SCORE].notnull()].copy()
+        unplayed = raw_data[raw_data[COLUMN_SCORE].isnull()].copy()
+        del raw_data
+
+        # split home & away for played games
+        goals = played[COLUMN_SCORE].str.split('–', expand=True)
+            
+        if not played.empty:
+            played[COLUMN_HOME_GOALS] = goals[0].astype(int)
+            played[COLUMN_HOME_GOALS] = goals[1].astype(int)
+        
+        del goals
+
+        if not unplayed.empty:
+            # unplayed games have NaN scores (to be predicted)
+            unplayed[COLUMN_HOME_GOALS] = np.nan
+            unplayed[COLUMN_AWAY_GOALS] = np.nan
+        
+        # clean
+        played.drop(columns=[COLUMN_SCORE], inplace=True)
+        unplayed.drop(columns=[COLUMN_SCORE], inplace=True)
+
+        logging.info("Successfully initialized played and unplayed tables")
+        return played, unplayed
     
-    # separate for processing scores
-    played = prem_data[prem_data['Score'].notnull()].copy()
-    unplayed = prem_data[prem_data['Score'].isnull()].copy()
+    except FileNotFoundError as e:
+        logging.error(f"File not found: {e}")
+        logging.error(traceback.format_exc())
 
-    print("played: \n", played.head())
-    print("unplayed: \n", played.head())
-    # split home & away for played games
+    except pd.errors.EmptyDataError as e:
+        logging.error(f"Empty data: {e}")
+        logging.error(traceback.format_exc())
 
-    goals = played['Score'].str.split('-', expand=True)
-    
-    # print("Split result:\n", goals.head())
-    
-    played.loc[:, 'Home Goals'] = goals[0].astype(int)
-    played.loc[:, 'Away Goals'] = goals[1].astype(int)
-
-    # unplayed games have NaN scores (to be predicted)
-    unplayed.loc[:, 'Home Goals'] = np.nan
-    unplayed.loc[:, 'Away Goals'] = np.nan
-    
-    # clean
-    played = played.drop(columns=['Score'])
-    unplayed = unplayed.drop(columns=['Score'])
-    del prem_data
-    del goals
-
-    return played, unplayed
-
-if __name__ == "__main__":
-    csv_path = 'data/prem_data.csv'
-    played, unplayed = clean_data(csv_path)
-    
-    # Optional: Print statements to verify the results
-    print(f"Played games:\n{played.head()}")
-    print(f"Unplayed games:\n{unplayed.head()}")
+    except Exception as e: 
+        logging.error(f"{e}")
+        logging.error(traceback.format_exc())
--- src/scrape_data.py
+++ src/scrape_data.py
@@ -1,69 +1,76 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-import os
+import logging
+import traceback
+
+# init logging
+logging.basicConfig(level = logging.INFO, format='%(asctime)s - %(levelname)s -  %(message)s')
+
+# define stat variables
+DATA_STAT_GAMEWEEK = 'gameweek'
+DATA_STAT_HOME_TEAM = 'home_team'
+DATA_STAT_HOME_XG = 'home_xg'
+DATA_STAT_SCORE = 'score'
+DATA_STAT_AWAY_XG = 'away_xg'
+DATA_STAT_AWAY_TEAM = 'away_team'
+DATA_STAT_MATCH_REPORT = 'match_report'
+
 
 # URL of the website containing the table
 def scrape_data(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception(f"Failed to retrieve webpage. Status code: {response.status_code}")
-    
-    # parse HTML content and find the table
-    soup = BeautifulSoup(response.content, 'html.parser')   
-    table = soup.find('table')        
-    if not table: 
-        raise ValueError("Table not found on webpage.")      
-        
-    # store scraped data in these lists
-    matchweek, home_team, home_xg, score, away_xg, away_team, match_reports = [], [], [], [], [], [], []
-
-    
-    # go through table -- exclude header row (tr = table row), start at 1: to exclude header
-    for row in table.find_all('tr')[1:]:
-        # Extract data from the cells using column names
-        cur_matchweek = row.find('th', {'data-stat': 'gameweek'}).text.strip()
-        cur_home_team = row.find('td', {'data-stat': 'home_team'}).text.strip()
-        cur_home_xg = row.find('td', {'data-stat': 'home_xg'}).text.strip()
-        cur_score = row.find('td', {'data-stat': 'score'}).text.strip()
-        cur_away_xg = row.find('td', {'data-stat': 'away_xg'}).text.strip()
-        cur_away_team = row.find('td', {'data-stat': 'away_team'}).text.strip()
-        cur_match_report_link = row.find('td', {'data-stat': 'match_report'}).find('a', href=True)
-        cur_match_report_url = cur_match_report_link['href'] if cur_match_report_link else ''
-        
-        # Append the extracted data to the lists
-        if cur_matchweek:
-            matchweek.append(cur_matchweek)
-            home_team.append(cur_home_team)
-            home_xg.append(cur_home_xg)
-            score.append(cur_score)
-            away_xg.append(cur_away_xg)
-            away_team.append(cur_away_team)
-            match_reports.append(cur_match_report_url)
-
-    return {
-        "Matchweek": matchweek,
-        "Home Team": home_team,
-        "Home XG": home_xg,
-        "Score": score,
-        "Away XG": away_xg,
-        "Away Team": away_team,
-        "Match Report": match_reports
-    }
-
-            
-if __name__ == "__main__":
     try: 
-        # get data from fbref
-        url = 'https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures'
-        data = scrape_data(url)
-        if not data: raise Exception("Data scrape failed")
+        # send a GET request to the URL
+        response = requests.get(url)
+        response.raise_for_status() # check HTTP errors
         
-        # scraped data -->  pandas dataframe --> csv
-        df = pd.DataFrame(data)
-        os.makedirs('data', exist_ok=True) # make directory if it doesn't exist
-        df.to_csv('data/prem_data.csv', index=False)
-        print("Data saved to data/prem_data.csv")
-    except Exception as e: 
-        print(f"ERROR: {e}")
+        
+        # parse HTML content and find the table
+        soup = BeautifulSoup(response.content, 'html.parser')   
+        table = soup.find('table')        
+        if not table: 
+            raise ValueError("Table not found on webpage.")      
+            
+        # store scraped data in these lists
+        matchweek, home_team, home_xg, score, away_xg, away_team, match_reports = [], [], [], [], [], [], []
+        
+        # go through table -- exclude header row (tr = table row), start at 1: to exclude header
+        for row in table.find_all('tr')[1:]:
+            # Extract data from the cells using column names
+            cur_matchweek = row.find('th', {'data-stat': DATA_STAT_GAMEWEEK}).text.strip()
+            cur_home_team = row.find('td', {'data-stat': DATA_STAT_HOME_TEAM}).text.strip()
+            cur_home_xg = row.find('td', {'data-stat': DATA_STAT_HOME_XG}).text.strip()
+            cur_score = row.find('td', {'data-stat': DATA_STAT_SCORE}).text.strip()
+            cur_away_xg = row.find('td', {'data-stat': DATA_STAT_AWAY_XG}).text.strip()
+            cur_away_team = row.find('td', {'data-stat': DATA_STAT_AWAY_XG}).text.strip()
+            cur_match_report_link = row.find('td', {'data-stat': DATA_STAT_MATCH_REPORT}).find('a', href=True)
+            cur_match_report_url = cur_match_report_link['href'] if cur_match_report_link else ''
+            
+            # Append the extracted data to the lists
+            if cur_matchweek:
+                matchweek.append(cur_matchweek)
+                home_team.append(cur_home_team)
+                home_xg.append(cur_home_xg)
+                score.append(cur_score)
+                away_xg.append(cur_away_xg)
+                away_team.append(cur_away_team)
+                match_reports.append(cur_match_report_url)
+
+        return {
+            "Matchweek": matchweek,
+            "Home Team": home_team,
+            "Home XG": home_xg,
+            "Score": score,
+            "Away XG": away_xg,
+            "Away Team": away_team,
+            "Match Report": match_reports
+        }
+
+    except requests.RequestException as e:
+        logging.error(f"ERROR: {e}")
+        logging.error(traceback.format_exc())
+        return None
+    except ValueError as e:
+        logging.error(f"ERROR: {e}")
+        logging.error(traceback.format_exc())
+        return None
