741a2d65da99b0dfdc2a76e8b81149d67a458163
balooop <pavan.baloo@gmail.com>
2024-06-24 22:15:56 -0400

    improved data scraping

--- src/scrape_data.py
+++ src/scrape_data.py
@@ -1,14 +1,21 @@
 import requests
-import pandas as pd
-from bs4 import BeautifulSoup
+from requests import RequestException 
+import yaml
 import logging
 import traceback
+import validators
+import os
 
-# init logging
-logging.basicConfig(level = logging.INFO, format='%(asctime)s - %(levelname)s -  %(message)s')
+import pandas as pd
+from bs4 import BeautifulSoup
+from typing import Dict, List, Optional
 
-# define stat variables
+# Configure logging to include the level, time, and message
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Define constants for data statistics
 DATA_STAT_GAMEWEEK = 'gameweek'
+DATA_STAT_DATE = 'date'
 DATA_STAT_HOME_TEAM = 'home_team'
 DATA_STAT_HOME_XG = 'home_xg'
 DATA_STAT_SCORE = 'score'
@@ -16,61 +23,78 @@ DATA_STAT_AWAY_XG = 'away_xg'
 DATA_STAT_AWAY_TEAM = 'away_team'
 DATA_STAT_MATCH_REPORT = 'match_report'
 
+def scrape_data(url: str) -> Optional[Dict[str,list]]:
+    """
+    Scrapes match data from a specified URL.
+    
+    Args:
+        url (str): URL to scrape data from.
 
-# URL of the website containing the table
-def scrape_data(url):
+    Returns:
+        dict: Dictionary containing scraped match data organized by categories.
+    """
+    # HTTP request to url
+    try:
+        with requests.get(url, timeout=10) as response:         # get request HTTP server for 10s
+            response.raise_for_status()                             # checks for errors
+    except RequestException as e:                               # catches all HTTP errors
+        logging.error(f"HTTP error occurred: {e}")
+        traceback.print_exc()
+        return None
+    
+    # parse table & store in table
     try: 
-        # send a GET request to the URL
-        response = requests.get(url)
-        response.raise_for_status() # check HTTP errors
+        # find the table in the webpage
+        soup = BeautifulSoup(response.content, 'html.parser')  
+        table = soup.find('table')
+        if not table:
+            raise ValueError("Table not found on webpage.")
         
-        
-        # parse HTML content and find the table
-        soup = BeautifulSoup(response.content, 'html.parser')   
-        table = soup.find('table')        
-        if not table: 
-            raise ValueError("Table not found on webpage.")      
-            
-        # store scraped data in these lists
-        matchweek, home_team, home_xg, score, away_xg, away_team, match_reports = [], [], [], [], [], [], []
-        
-        # go through table -- exclude header row (tr = table row), start at 1: to exclude header
-        for row in table.find_all('tr')[1:]:
-            # Extract data from the cells using column names
-            cur_matchweek = row.find('th', {'data-stat': DATA_STAT_GAMEWEEK}).text.strip()
-            cur_home_team = row.find('td', {'data-stat': DATA_STAT_HOME_TEAM}).text.strip()
-            cur_home_xg = row.find('td', {'data-stat': DATA_STAT_HOME_XG}).text.strip()
-            cur_score = row.find('td', {'data-stat': DATA_STAT_SCORE}).text.strip()
-            cur_away_xg = row.find('td', {'data-stat': DATA_STAT_AWAY_XG}).text.strip()
-            cur_away_team = row.find('td', {'data-stat': DATA_STAT_AWAY_XG}).text.strip()
-            cur_match_report_link = row.find('td', {'data-stat': DATA_STAT_MATCH_REPORT}).find('a', href=True)
-            cur_match_report_url = cur_match_report_link['href'] if cur_match_report_link else ''
-            
-            # Append the extracted data to the lists
-            if cur_matchweek:
-                matchweek.append(cur_matchweek)
-                home_team.append(cur_home_team)
-                home_xg.append(cur_home_xg)
-                score.append(cur_score)
-                away_xg.append(cur_away_xg)
-                away_team.append(cur_away_team)
-                match_reports.append(cur_match_report_url)
-
-        return {
-            "Matchweek": matchweek,
-            "Home Team": home_team,
-            "Home XG": home_xg,
-            "Score": score,
-            "Away XG": away_xg,
-            "Away Team": away_team,
-            "Match Report": match_reports
+        # initialize list to store data
+        data: Dict[str, List[str]] = {
+            "Matchweek": [], "Date": [], "Home Team": [], "Home XG": [],
+            "Score": [], "Away XG": [], "Away Team": [], "Match Report": []
         }
 
-    except requests.RequestException as e:
-        logging.error(f"ERROR: {e}")
-        logging.error(traceback.format_exc())
+        cur_matchweek, cur_date = "", ""
+        # process rows in the webpage (start at 1: to exclude header)
+        for row in table.find_all('tr')[1:]:
+            # special cases, matchweek and date
+            matchweek = row.find('th', {'data-stat': DATA_STAT_GAMEWEEK}).text.strip() 
+            date = row.find('td', {'data-stat': DATA_STAT_DATE}).text.strip() 
+            # if we find a matchweek / date, set them. otherwise, if blank, use previous
+            cur_matchweek = matchweek if matchweek else cur_matchweek
+            cur_date = date if date else cur_date 
+            data["Matchweek"].append(cur_matchweek) 
+            data["Date"].append(cur_date)
+
+            # standard fields
+            data["Home Team"].append(row.find('td', {'data-stat': DATA_STAT_HOME_TEAM}).text.strip())
+            data["Home XG"].append(row.find('td', {'data-stat': DATA_STAT_HOME_XG}).text.strip())
+            data["Score"].append(row.find('td', {'data-stat': DATA_STAT_SCORE}).text.strip())
+            data["Away XG"].append(row.find('td', {'data-stat': DATA_STAT_AWAY_XG}).text.strip())
+            data["Away Team"].append(row.find('td', {'data-stat': DATA_STAT_AWAY_TEAM}).text.strip())
+            
+            # special case, match report link -- append URL if found
+            match_report_link = row.find('td', {'data-stat': DATA_STAT_MATCH_REPORT}).find('a', href=True)
+            data["Match Report"].append(match_report_link['href'] if match_report_link else '')
+            
+        logging.info("Data scraped successfully")
+        return data
+    
+    # exception handling
+    except Exception as e:                                      
+        logging.error(f"An error occurred: {e}")
+        traceback.print_exc()
         return None
-    except ValueError as e:
-        logging.error(f"ERROR: {e}")
-        logging.error(traceback.format_exc())
-        return None
+
+if __name__ == "__main__":
+    # load url from config
+    url = 'https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures'
+    # test
+    result = scrape_data(url)
+    if result:
+        df = pd.DataFrame(result)
+        print("success\n", df.head, "\nshape:", df.shape)
+    else:
+        print("Data scraping failed")
